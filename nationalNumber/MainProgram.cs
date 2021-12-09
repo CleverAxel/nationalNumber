ce5fc20435779e2b6a460521e8e4f493090eb737
CleverAxel <90392657+CleverAxel@users.noreply.github.com>
2021-12-09 10:18:20 +0100

    -

---
+++ nationalNumber/MainProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using LibNatNumber;
+
+namespace nationalNumber {
+    internal class MainProgram {
+        static void Main(string[] args) {
+
+            //CORRECT
+            NationalNumber.IsValid("42.01.22.051-81");
+
+            NationalNumber.IsValid("17 07 30 033 84");
+
+            NationalNumber.IsValid("00 00 01 123 41");
+
+            //INCORRECT
+            //NationalNumber.IsValid("44 02 29 033 84");
+        }
+    }
+}
